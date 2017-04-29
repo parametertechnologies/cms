a6e222387431d037deffefaaaaa81add83965329
CarlosGabaldon <cgabaldon@gmail.com>
2017-04-28 19:28:05 -0700

    Complete the 'D' in CRUD'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -33,8 +37,10 @@ class ArticlesController < ApplicationController
 
   end
 
-  def show
+  def destroy
     @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
