b4cd7e4c3db6abddd10ac03cfd760aca5d4f0e71
CarlosGabaldon <cgabaldon@gmail.com>
2017-05-01 15:34:16 -0700

    Editing comments is bit hacky in the articles#show view since I have not implemented inline ajax for editing. For now comments controller just passes the comment being edited and renders the articles#show in the comments#edit action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,36 @@
 class CommentsController < ApplicationController
+
+  # /articles/:article_id/comments/new
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  # /articles/:article_id/comments/:id/edit
+  def edit
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    render 'articles/show'
+
+  end
+
+  # /articles/:article_id/comments/:id
+  def update
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to article_path(@article)
+    end
+
+  end
+
+  # /articles/:article_id/comments/:id
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
 
