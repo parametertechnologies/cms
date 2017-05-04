09effabe1a5700aeacd1fb994bdfdfa10aa48415
CarlosGabaldon <cgabaldon@gmail.com>
2017-05-03 17:02:03 -0700

    test comment validations and be consistent with using single quotes with strings if not doing string interpolation

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test "should not save article without title" do
+  test 'should not save article without title' do
     article = Article.new
-    assert_not article.save, "Article saved without title"
+    assert_not article.save, 'Article saved without title'
   end
 
   test 'should not have article title less than 5' do
     article = Article.new
-    article.title = "S"
-    assert_not article.save, "Article title saved with minimum < 5 "
+    article.title = 'S'
+    assert_not article.save, 'Article title saved with minimum < 5 '
   end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save comment without commenter' do
+    comment = new_comment(new_article)
+    assert_not comment.save, 'Comment saved without commenter'
+  end
+
+  test 'should not save comment without body' do
+    comment = new_comment(new_article)
+    comment.commenter = 'The Dude'
+    assert_not comment.save, 'Comment saved without body'
+  end
+
+  private
+  def new_article
+    article = Article.create(title: 'New Article', text: 'Article Text')
+  end
+
+  def new_comment(article)
+    article.comments.build
+  end
 end
