11b57f7116df67422e6f1cfadcf3e93dd9c0c252
CarlosGabaldon <cgabaldon@gmail.com>
2017-05-10 16:39:51 -0700

    logged in users should be able to edit articles

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -9,11 +9,9 @@ class ArticlesTest < ApplicationSystemTestCase
   end
 
   test 'creating a new article' do
-    visit articles_url
-    click_link 'Log in'
     login_in_user
     visit articles_url
-    
+
     click_link 'New Article'
     fill_in 'Title', with: 'Exciting Article'
     fill_in 'Text', with: 'This is a new exciting article'
@@ -21,8 +19,23 @@ class ArticlesTest < ApplicationSystemTestCase
     assert_selector 'h1', text: 'Exciting Article'
   end
 
+  test 'editing a article' do
+    login_in_user
+    visit articles_url
+    article = articles(:one)
+    within("#article_#{article.id}") do
+      click_link 'Edit'
+    end
+
+    fill_in 'Title', with: 'Amazing Article'
+    click_button 'Update Article'
+    assert_selector 'h1', text: 'Amazing Article'
+  end
+
   private
   def login_in_user
+    visit articles_url
+    click_link 'Log in'
     fill_in 'Email', with: 'jason@email.com'
     fill_in 'Password', with: '123greetings'
     click_button 'Log in'
